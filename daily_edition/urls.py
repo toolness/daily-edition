d62a4fe51184e2b03cea9b02d49a035c8a6a3f82
Atul Varma <varmaa@gmail.com>
2011-04-19 14:50:20 -0700

    Added publish and login views.

---
+++ daily_edition/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'de.views.home', name='home'),
+    # url(r'^de/', include('de.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^publish/', 'daily_edition.views.publish_edition'),
+)
--- daily_edition/views.py
+++ daily_edition/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+import threading
+
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+from django.core import management
+
+@login_required
+def publish_edition(req):
+    if req.method == 'POST':
+        if 'refresh-feeds' in req.POST:
+            options = dict(update_urls=True)
+            response = ('Starting the presses. This may take a while, '
+                        'since feeds are being refreshed.')
+        else:
+            options = {}
+            response = 'Starting the presses.'
+        
+        def start_job():
+            management.call_command('publish_edition', **options)
+
+        t = threading.Thread(target=start_job)
+        t.start()
+    else:
+        response = ''
+    context = dict(response=response)
+    context.update(csrf(req))
+    return render_to_response('daily_edition/publish_edition.html',
+                              context)
