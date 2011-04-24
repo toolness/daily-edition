31977459da40b09753920eb1fb4dd23ae4fdc120
Atul Varma <varmaa@gmail.com>
2011-04-23 19:05:01 -0700

    Added link to latest edition from index (publish) page.

--- daily_edition/urls.py
+++ daily_edition/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'daily_edition.views.publish_edition'),
-    url(r'^issue/latest/$', 'daily_edition.views.edition'),
+    url(r'^issue/latest/$', 'daily_edition.views.edition',
+        name='latest-edition'),
     url(r'^issue/(?P<issue>\d+)/$', 'daily_edition.views.edition'),
 )
